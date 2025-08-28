29710f0f7766b474045c7fdcd2c263365c3830c3
Juan Luguito <juanmet1234@gmail.com>
2025-08-28 01:04:15 -0500

    Agrego index.html, style.css de la pagina de navegacion

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask import render_template  
+app = Flask(__name__)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hola desde app.py'
+    return render_template('index.html') 
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
