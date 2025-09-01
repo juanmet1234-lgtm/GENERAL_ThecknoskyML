c5b9b57a30b333797104b066a7c9c1917e1c10f0
Juan Luguito <juanmet1234@gmail.com>
2025-09-01 18:37:14 -0500

    Guardando mis cambios locales antes de revertir

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask
 from flask import render_template  
 app = Flask(__name__)
 
-app = Flask(__name__)
-
 @app.route('/')
 def home():
     return render_template('index.html') 
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/PagLugo')
+def pag_lugo():
+    return render_template('PagLugo.html')
+
 
 
 
