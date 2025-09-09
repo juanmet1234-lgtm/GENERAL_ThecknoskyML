6118e213541c9554169be2b1e41be3f33b62e76c
Juan Luguito <juanmet1234@gmail.com>
2025-09-09 14:59:24 -0500

    Agrego Menu principal y menu de A4 ademas del .py principal del trabajo SIN EL RESULTADO PROPUESTO POR EL DOCENTE

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
-from flask import render_template  
+from flask import render_template, request  
+from LinearRegression import calculateGrade
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html') 
+#TRABAJO 1 ----------------------------------------------------------------------------------
+@app.route('/MenuMLcasos')
+def MenuMLcasos():
+    return render_template('MenuMLcasos.html')
+
 
 @app.route('/FiltradoDeCorreos')
+def Filtrado_de_correos():
+    return render_template('FiltradoDeCorreos.html')
+
 def Filtrado_de_correos():
     return render_template('FiltradoDeCorreos.html')
 
@@ -18,16 +28,25 @@ def FraudesBancarios():
 def pag_lugo():
     return render_template('PagLugo.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
-
-
-
-# Ruta para la página de Camero
 @app.route('/PagCamero')
 def pag_camero():
     return render_template('PagCamero.html')
+
+#TRABAJO 2 ----------------------------------------------------------------------------------
+
+@app.route('/MunuLR')
+def Menu_LR():
+    return render_template('MenuLR.html')
+
+@app.route('/LR', methods=['GET', 'POST'])
+def Lr():
+    calculateResult = None
+    if request.method == 'POST':
+        hours = float(request.form['hours'])
+        calculateResult = calculateGrade(hours) 
+    return render_template('LR.html', result=calculateResult)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
